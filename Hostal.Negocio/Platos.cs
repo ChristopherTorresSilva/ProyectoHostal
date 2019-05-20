2990ec108da8944df831c7c49d2d488b2405a5d4
maurottroncosoo <maurottroncosoo@gmail.com>
2019-05-19 22:36:06 -0400

    clase platos

---
+++ Hostal.Negocio/Platos.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using System.Data;
+
+namespace Hostal.Negocio
+{
+    public class Platos
+    {
+        OracleConnection OracleCon = new OracleConnection();
+        public Platos()
+        {
+            try
+            {
+                Conexion con = new Conexion();
+
+                OracleCon = con.Get_Connecion();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+
+        }
+
+        public Platos(string sqlcon)
+        {
+            try
+            {
+                OracleCon = new OracleConnection(sqlcon);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+
+        public bool CreatePlatos(int newprecio,
+            int newtiposervicio)
+        {
+            //System.Diagnostics.Debug.WriteLine(userN);
+            DataTable dt = new DataTable();
+            try
+            {
+
+                //
+                OracleCommand _OracleCommand = new OracleCommand();
+                _OracleCommand.CommandText = "INSERTAR_PLATO";
+                _OracleCommand.Connection = OracleCon;
+                _OracleCommand.CommandType = CommandType.StoredProcedure;
+
+                //OracleCommand comando = new OracleCommand("INSERTAR_USUARIO_EMPLEADO", OracleCon);
+                _OracleCommand.CommandType = CommandType.StoredProcedure;
+                _OracleCommand.Parameters.Add("newprecio", OracleDbType.Int32).Value = newprecio;
+                _OracleCommand.Parameters.Add("newtiposervicio", OracleDbType.Int32).Value = newtiposervicio;
+
+
+
+                //_OracleCommand.ExecuteNonQuery();
+                OracleCon.Open();
+                OracleDataAdapter _OracleDataAdapter = new OracleDataAdapter(_OracleCommand);
+                //_OracleDataAdapter.SelectCommand = _OracleCommand;
+                _OracleDataAdapter.Fill(dt);
+                System.Diagnostics.Debug.WriteLine(dt.Rows.Count);
+                OracleCon.Close();
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //System.Diagnostics.Debug.WriteLine(userN);
+                return false;
+            }
+        }
+
+
+
+
+
+    }
+}
